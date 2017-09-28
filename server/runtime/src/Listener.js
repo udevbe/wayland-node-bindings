4da2d4163ca3c095a209105be60800916719e870
Erik De Rijcke <derijcke.erik@gmail.com>
2017-09-28 15:10:43 +0200

    keep ref of func pointer as well as listener itself

--- server/runtime/src/Listener.js
+++ server/runtime/src/Listener.js
@@ -5,22 +5,39 @@ const WlListener = native.structs.wl_listener.type
 
 class Listener {
   static create (func) {
+    const funcPtr = native.interface.wl_notify_func_t(func)
     const struct = new WlListener({
       link: null,
-      notify: native.interface.wl_notify_func_t(func)
+      notify: funcPtr
     })
 
-    return new Listener(struct)
+    const listener = new Listener(struct, funcPtr)
+    this._ref(listener)
+    return listener
   }
 
-  constructor (struct) {
+  static _ref (listener) {
+    Listener._refs.push(listener)
+  }
+
+  constructor (struct, funcPtr) {
     this._struct = struct
+    this._funcPtr = funcPtr
   }
 
   get ptr () {
     return this._struct.ref()
   }
+
+  unref () {
+    const index = Listener._refs.indexOf(this)
+    if (index > -1) {
+      Listener._refs.splice(index, 1)
+    }
+  }
 }
+
+Listener._refs = []
 
 require('./namespace').wl_listener = Listener
 module.exports = Listener
