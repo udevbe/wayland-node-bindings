7198aa78211502c8e6f4f56696aff089b33a5e30
Erik De Rijcke <derijcke.erik@gmail.com>
2017-09-29 22:22:27 +0200

    keep ref to allocated buffers to avoid gc

--- server/runtime/src/Interface.js
+++ server/runtime/src/Interface.js
@@ -7,18 +7,22 @@ const WlInterface = wlServerCore.structs.wl_interface.type
 
 class Interface {
   static create (name, version, methods, events) {
+    const namePtr = fastcall.makeStringBuffer(name)
     return new Interface(new WlInterface({
-      name: fastcall.makeStringBuffer(name),
+      name: namePtr,
       version: version,
       method_count: methods.length,
       methods: methods,
       event_count: events.length,
       events: events
-    }))
+    }), namePtr, methods, events)
   }
 
-  constructor (struct) {
+  constructor (struct, namePtr, methods, events) {
     this._struct = struct
+    this._namePtr = namePtr
+    this._methods = methods
+    this._events = events
   }
 
   get ptr () {
