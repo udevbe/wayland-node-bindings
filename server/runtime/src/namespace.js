8be4b5c7d9a463fcd826f99ce135575574416b42
Erik De Rijcke <derijcke.erik@gmail.com>
2017-10-20 23:02:17 +0200

    remove ref calls from argument builderes

--- server/runtime/src/namespace.js
+++ server/runtime/src/namespace.js
@@ -6,19 +6,19 @@ const WlArray = require('./WlArray')
 
 module.exports = {
   _uintOptional: (arg) => {
-    return new WlArgument({u: arg === null ? null : arg}).ref()
+    return new WlArgument({u: arg === null ? null : arg})
   },
 
   _uint: (arg) => {
-    return new WlArgument({u: arg}).ref()
+    return new WlArgument({u: arg})
   },
 
   _fdOptional: (arg) => {
-    return new WlArgument({h: arg === null ? null : arg}).ref()
+    return new WlArgument({h: arg === null ? null : arg})
   },
 
   _fd: (arg) => {
-    return new WlArgument({h: arg}).ref()
+    return new WlArgument({h: arg})
   },
 
   _intOptional: (arg) => {
@@ -30,38 +30,38 @@ module.exports = {
   },
 
   _fixedOptional: (arg) => {
-    return new WlArgument({f: arg === null ? null : arg._raw}).ref()
+    return new WlArgument({f: arg === null ? null : arg._raw})
   },
 
   _fixed: (arg) => {
-    return new WlArgument({f: arg._raw}).ref()
+    return new WlArgument({f: arg._raw})
   },
 
   _objectOptional: (arg) => {
-    return new WlArgument({o: arg === null ? null : arg.ptr}).ref()
+    return new WlArgument({o: arg === null ? null : arg.ptr})
   },
 
   _object: (arg) => {
-    return new WlArgument({o: arg}).ref()
+    return new WlArgument({o: arg})
   },
 
   _newObject: (arg) => {
-    return new WlArgument({n: arg.id}).ref()
+    return new WlArgument({n: arg.id})
   },
 
   _stringOptional: (arg) => {
-    return new WlArgument({s: arg === null ? null : fastcall.makeStringBuffer(arg)}).ref()
+    return new WlArgument({s: arg === null ? null : fastcall.makeStringBuffer(arg)})
   },
 
   _string: (arg) => {
-    return new WlArgument({s: fastcall.makeStringBuffer(arg)}).ref()
+    return new WlArgument({s: fastcall.makeStringBuffer(arg)})
   },
 
   _arrayOptional: (arg) => {
-    return new WlArgument({a: arg === null ? null : WlArray.create(arg).ptr}).ref()
+    return new WlArgument({a: arg === null ? null : WlArray.create(arg).ptr})
   },
 
   _array: (arg) => {
-    return new WlArgument({a: WlArray.create(arg).ptr}).ref()
+    return new WlArgument({a: WlArray.create(arg).ptr})
   }
 }
